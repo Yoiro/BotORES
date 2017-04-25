c046f3c7c17308dd5e5eb713f05d9907a429f80a
Yoiro <degrevesim@gmail.com>
2017-04-25 10:48:11 +0200

    setuptools ok
    prochaine étape: distribution sur le NAS

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='oresbot',
     version='1.0',
     description='Téléchargement auto depuis le site d\'ORES',
     author='Simon De Greve',
-    packages=['webdriver'],
-    install_requires=[
-        'selenium',
-    ]
+    packages = find_packages(),
+    scripts = ['oresbot/download_ores.py'],
+    install_requires = [
+        'appdirs>=1.4.3',
+        'distlib>=0.2.4',
+        'future>=0.16.0',
+        'packaging>=16.8',
+        'pyparsing>=2.2.0',
+        'pypiwin32>=220',
+        'selenium>=3.3.3',
+        'six>=1.10.0',
+    ],
+    package_data={
+        '':['geckodriver', '*.tar.gz', '*.zip'],
+        'oresbot':['*.json'],
+    }
 )
