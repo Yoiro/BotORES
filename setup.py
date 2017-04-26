2f9f7ccaf8507eb33f1447163daa9bf20008a5f5
Yoiro <degrevesim@gmail.com>
2017-04-26 14:27:57 +0200

    qegq

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'distlib>=0.2.4',
         'future>=0.16.0',
         'packaging>=16.8',
-        'pyparsing>=2.2.0', 
+        'pyparsing>=2.2.0',
         'pypiwin32>=220',
         'selenium>=3.3.3',
         'six>=1.10.0',
